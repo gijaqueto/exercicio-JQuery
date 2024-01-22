550d1298ade737cc99514c5055589e803cd93eea
Giovanna Jaqueto <gijaqueto1@gmail.com>
2024-01-21 21:38:15 -0300

    210124

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
     $('form').on('submit', function (e) {
         e.preventDefault();
-        const novoItemdaLista = $('tarefa-nova').val();
+        const novoItemdaLista = $('#tarefa-nova').val();
         const novoItem = $('<li></li>');
         $(`<p>${novoItemdaLista}</p>`).appendTo(novoItem);
         $(novoItem).appendTo('ul');
         $('#tarefa-nova').val('');
+    })
+
+    $("ul").on("click", "li", function () {
+        const item = $('#itens').val();
+        const itemRiscado = $('<li></li>');
+        $(`<p style = text-decoration: line-through>${item}</p>`).appendTo(itemRiscado);
     })
 })
